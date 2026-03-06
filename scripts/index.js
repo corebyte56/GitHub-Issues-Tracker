63954dda77be6c387e5a5d87c981175471c371d8
corebyte56 <fahim789m@gmail.com>
2026-03-07 01:00:25 +0600

    feat: add login authentication logic with redirect and validation

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+const userName = document.getElementById("username");
+const password = document.getElementById("password");
+const loginBtn = document.getElementById("login-btn");
+
+loginBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const usernameValue = userName.value.trim();
+    const passwordValue = password.value.trim();
+
+    if (usernameValue === "admin" && passwordValue === "admin123") {
+        // Redirect to home page
+        alert("Login successful! Redirecting to home page...");
+        window.location.href = "./home.html";
+    }
+
+    else if (usernameValue === "" || passwordValue === "") {
+        alert("Please enter both username and password.");
+    }
+
+    else {
+        alert("Invalid username or password. Please try again.");
+    }
+})

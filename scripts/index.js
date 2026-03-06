8d14d9bad25b7e21a6e1563ad79717d7fcf71a63
corebyte56 <fahim789m@gmail.com>
2026-03-07 01:04:37 +0600

    feat(login): enhance authentication validation and error handling

--- scripts/index.js
+++ scripts/index.js
@@ -6,12 +6,20 @@ loginBtn.addEventListener('click', (e) => {
     e.preventDefault();
 
     const usernameValue = userName.value.trim();
-    const passwordValue = password.value.trim();
+    const passwordValue = password.value;
 
     if (usernameValue === "admin" && passwordValue === "admin123") {
         // Redirect to home page
         alert("Login successful! Redirecting to home page...");
         window.location.href = "./home.html";
+    }
+
+    else if (usernameValue === "admin" && passwordValue !== "admin123") {
+        alert("Incorrect password. Please try again.");
+    }
+
+    else if (usernameValue !== "admin" && passwordValue === "admin123") {
+        alert("Incorrect username. Please try again.");
     }
 
     else if (usernameValue === "" || passwordValue === "") {
